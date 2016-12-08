b25b7427154b652b74b0877103ffe11e89e02c93
Gene Anthony Kadano <gskadano@gmail.com>
2016-12-08 22:27:03 +0800

    Update unique path for backend and frontend index.php

--- Application/BIR/backend/config/main.php
+++ Application/BIR/backend/config/main.php
@@ -36,7 +36,7 @@ return [
 			'showScriptName' => false,
 			//'baseUrl' => 'http://localhost/birproj/frontend/web/index.php',
 			//'baseUrl' => 'http://localhost\..\frontend/web/index.php',
-			'baseUrl' => 'birdoctrackingsample.16mb.com',
+			'baseUrl' => 'birdoctrackingsample.16mb.com/Application/BIR/frontend/web/index.php',
 		],
     ],
     'params' => $params,
